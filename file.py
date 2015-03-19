8fa07086ab09aa71a9fffbe42a3a3a8fb590cb81
suzuki <takeo_suzuki@kronos-jp.net>
2015-03-19 17:00:23 +0900

    source image for

--- file.py
+++ file.py
@@ -9,32 +9,32 @@ f.close()
 # lines: リスト。要素は1行の文字列データ
 
 for line in lines:
-    words = line.split()
-    fileName = words[0]
-    fileNameNoExt = fileName.split(".")[0]
-    fileNameExt = fileName.split(".")[1]
-    fileTag = words[1]
+    fileName = line.split()[0]
+    fileTag = line.split()[1]
+    fileNameNoExt = fileName.split(".")[0] + "_"
+    fileNameExt = "." + fileName.split(".")[1]
     print fileName
+    print fileTag
     print fileNameNoExt
     print fileNameExt
-    print fileTag
-    
+
+    imageFiles = [fileNameNoExt]
     # trinmming face
-    fileName_f = fileNameNoExt + "_f." + fileNameExt
+    fileName_f = fileNameNoExt + "f" + fileNameExt
     image_f = faceTools.face_detect(fileName)
     cv2.imwrite(fileName_f,image_f)
+    imageFiles.append(fileName_f)
     
     # histgram equalize
-    fileName_e = fileNameNoExt + "_e." + fileNameExt
-    image_e = faceTools.equalize(fileName)
-    cv2.imwrite(fileName_e,image_e)
-    
-    # histgram equalize
-    fileName_fe = fileNameNoExt + "_fe." + fileNameExt
-    image_fe = faceTools.equalize(fileName_f)
-    cv2.imwrite(fileName_fe,image_fe)
+    for fn in list(imageFiles):
+        fileName_e = fn + "e" + fileNameExt
+        image_e = faceTools.equalize(fn)
+        cv2.imwrite(fileName_e,image_e)
+        imageFiles.append(fileName_e)
     
     # reverseLR
-    fileName_r = fileNameNoExt + "_r." + fileNameExt
-    image_r = faceTools.reverseLR(fileName)
-    cv2.imwrite(fileName_r,image_r)
+    for fn in list(imageFiles):
+        fileName_r = fn + "r" + fileNameExt
+        image_r = faceTools.reverseLR(fileName)
+        cv2.imwrite(fileName_r,image_r)
+        imageFiles.append(fileName_r)
