4a33a9a426f16c1f0241008cb0d5fef4f4b9eb29
kronos <takeo_suzuki@kronos-jp.net>
2015-03-20 19:55:10 +0900

    create prototxt.

--- match.py
+++ match.py
@@ -7,7 +7,7 @@ argvs = sys.argv  # コマンドライン引数を格納したリストの取得
 argc = len(argvs) # 引数の個数
 
 if ( argc < 2 ):
-net = caffe.Classifier('_deploy.prototxt','_iter_10000.caffemodel', image_dims = (33, 33))
+net = caffe.Classifier('deploy.prototxt','_iter_10000.caffemodel', image_dims = (277, 277))
 net.set_phase_test()# test phaseで定義されたcaffe netを使用する
 net.set_raw_scale('data', 255)  # data layerに入力として与えられる画像の輝度上限を指定
 
