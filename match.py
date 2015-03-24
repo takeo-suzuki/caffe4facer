7c96ebfd9f52f203beb49552ec298577d856121b
kronos <takeo_suzuki@kronos-jp.net>
2015-03-24 20:32:35 +0900

    change match.py and train settings.

--- match.py
+++ match.py
@@ -6,10 +6,9 @@ import sys, cv2, faceTools, caffe
 argvs = sys.argv  # コマンドライン引数を格納したリストの取得
 argc = len(argvs) # 引数の個数
 
-if ( argc < 2 ):
-net = caffe.Classifier('deploy.prototxt','_iter_10000.caffemodel', image_dims = (277, 277))
-net.set_phase_test()# test phaseで定義されたcaffe netを使用する
-net.set_raw_scale('data', 255)  # data layerに入力として与えられる画像の輝度上限を指定
-
-scores = net.predict([caffe.io.load_image(argvs[1], color = False, )], oversample=False)
-print scores
+if ( argc > 1 ):
+    net = caffe.Classifier('deploy.prototxt','fr_train_iter_100.caffemodel', image_dims = (256, 256))
+    #net.set_phase_test()# test phaseで定義されたcaffe netを使用する
+    #net.set_raw_scale('data', 255)  # data layerに入力として与えられる画像の輝度上限を指定
+    scores = net.predict([caffe.io.load_image(argvs[1], color = False, )], oversample=False)
+    print scores
